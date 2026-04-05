0c3d3755b9d9d52a58be673e21d58607f4e333b7
Elnix90 <elnix91@proton.me>
2026-04-05 10:11:10 +0200

    fix: removed local storage entry, bc it is not working on gh pages

--- site/src/jsMain/kotlin/io/github/elnix90/hackdroid/AppEntry.kt
+++ site/src/jsMain/kotlin/io/github/elnix90/hackdroid/AppEntry.kt
@@ -1,7 +1,6 @@
 package io.github.elnix90.hackdroid
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import com.varabyte.kobweb.compose.css.ScrollBehavior
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.minHeight
@@ -15,16 +14,13 @@ import com.varabyte.kobweb.silk.init.registerStyleBase
 import com.varabyte.kobweb.silk.style.common.SmoothColorStyle
 import com.varabyte.kobweb.silk.style.toModifier
 import com.varabyte.kobweb.silk.theme.colors.ColorMode
-import com.varabyte.kobweb.silk.theme.colors.loadFromLocalStorage
-import com.varabyte.kobweb.silk.theme.colors.saveToLocalStorage
 import com.varabyte.kobweb.silk.theme.colors.systemPreference
 import org.jetbrains.compose.web.css.vh
 
-private const val COLOR_MODE_KEY = "maker:colorMode"
 
 @InitSilk
 fun initColorMode(ctx: InitSilkContext) {
-    ctx.config.initialColorMode = ColorMode.loadFromLocalStorage(COLOR_MODE_KEY) ?: ColorMode.systemPreference
+    ctx.config.initialColorMode = ColorMode.systemPreference
 }
 
 @InitSilk
@@ -38,10 +34,6 @@ fun initStyles(ctx: InitSilkContext) {
 @Composable
 fun AppEntry(content: @Composable () -> Unit) {
     SilkApp {
-        val colorMode = ColorMode.current
-        LaunchedEffect(colorMode) {
-            colorMode.saveToLocalStorage(COLOR_MODE_KEY)
-        }
         Surface(SmoothColorStyle.toModifier().minHeight(100.vh)) {
             content()
         }
--- site/src/jsMain/kotlin/io/github/elnix90/hackdroid/components/sections/NavHeader.kt
+++ site/src/jsMain/kotlin/io/github/elnix90/hackdroid/components/sections/NavHeader.kt
@@ -1,27 +1,14 @@
 package io.github.elnix90.hackdroid.components.sections
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-import com.varabyte.kobweb.browser.dom.ElementTarget
 import com.varabyte.kobweb.compose.foundation.layout.Row
-import com.varabyte.kobweb.compose.foundation.layout.Spacer
-import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.fillMaxWidth
-import com.varabyte.kobweb.compose.ui.modifiers.gap
 import com.varabyte.kobweb.silk.components.graphics.Image
-import com.varabyte.kobweb.silk.components.icons.MoonIcon
-import com.varabyte.kobweb.silk.components.icons.SunIcon
 import com.varabyte.kobweb.silk.components.navigation.Link
-import com.varabyte.kobweb.silk.components.overlay.PopupPlacement
-import com.varabyte.kobweb.silk.components.overlay.Tooltip
 import com.varabyte.kobweb.silk.style.CssStyle
 import com.varabyte.kobweb.silk.style.base
 import com.varabyte.kobweb.silk.style.toModifier
-import com.varabyte.kobweb.silk.theme.colors.ColorMode
-import io.github.elnix90.hackdroid.components.widgets.IconButton
-import org.jetbrains.compose.web.css.cssRem
 
 val NavHeaderStyle = CssStyle.base {
     Modifier.fillMaxWidth()
@@ -29,29 +16,11 @@ val NavHeaderStyle = CssStyle.base {
 
 
 @Composable
-private fun ColorModeButton() {
-    var colorMode by ColorMode.currentState
-    IconButton(onClick = { colorMode = colorMode.opposite }) {
-        if (colorMode.isLight) MoonIcon() else SunIcon()
-    }
-    Tooltip(ElementTarget.PreviousSibling, "Toggle color mode", placement = PopupPlacement.BottomRight)
-}
-
-@Composable
 fun NavHeader(modifier: Modifier) {
     Row(NavHeaderStyle.toModifier().then(modifier)) {
         Link("https://ysws.hackclub.com/") {
             // Block display overrides inline display of the <img> tag, so it calculates centering better
             Image("flag-orpheus-top.svg", "Hackclub logo")
-        }
-
-        Spacer()
-
-        Row(
-            Modifier.gap(1.5.cssRem),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            ColorModeButton()
         }
     }
 }
